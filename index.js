782a43f528eec55aba6d690b2f2337425e8214aa
physcorgi <daniilglad@me.com>
2025-10-04 11:32:03 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('keydown', function (event) {
+
+})
+
+document.removeEventListener('keydown', function (event) {
+
+})
+
+document.dispatchEvent(new CustomEvent('click', {
+
+}))
